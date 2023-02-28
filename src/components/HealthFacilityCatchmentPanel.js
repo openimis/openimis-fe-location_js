c3ce76be15943974d58945807459e7fec5ef3d7a
lruzicki <lukasz.ruz@gmail.com>
2023-02-28 15:57:04 +0100

    OTC-838: rever otc 770 changes

--- src/components/HealthFacilityCatchmentPanel.js
+++ src/components/HealthFacilityCatchmentPanel.js
@@ -40,7 +40,6 @@ class HealthFacilityCatchmentPanel extends FormPanel {
     l2s: [],
     l3: null,
     l3s: [],
-    userL0s: [],
   };
 
   constructor(props) {
@@ -61,7 +60,6 @@ class HealthFacilityCatchmentPanel extends FormPanel {
         l1s: this.props.l1s,
         l2s: this.props.l2s,
         l3s: this.props.l3s,
-        userL0s: this.props.userL0s,
       });
     } else if (prevState.l0 !== this.state.l0) {
       if (!this.state.l0) {
@@ -155,8 +153,7 @@ class HealthFacilityCatchmentPanel extends FormPanel {
       errorL3s,
       readOnly,
     } = this.props;
-    const { l0s, l1s, l2s, l3s, userL0s } = this.state;
-    console.log(userL0s);
+    const { l0s, l1s, l2s, l3s } = this.state;
     return (
       <Grid container spacing={1} className={classes.item}>
         <Grid item xl={8} lg={12}>
@@ -171,7 +168,7 @@ class HealthFacilityCatchmentPanel extends FormPanel {
                 fetched={fetchedL0s}
                 error={errorL0s}
                 location={this.state.l0}
-                locations={this.state.userL0s}
+                locations={l0s}
                 currentParents={this.state.currentParents}
                 stateLocation={this.state.location}
                 reassign={true}
@@ -256,7 +253,6 @@ const mapStateToProps = (state) => ({
   fetchedL3s: state.loc.fetchedL3s,
   l3s: state.loc.l3s,
   errorL3s: state.loc.errorL3s,
-  userL0s: state.loc.userL0s,
 });
 
 const mapDispatchToProps = (dispatch) => {
