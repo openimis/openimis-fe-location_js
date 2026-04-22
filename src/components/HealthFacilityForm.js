83cf6c739601068cb09cec12fcff178b2adf702c
vaneck237 <vaneckbilongo@gmail.com>
2026-04-22 13:19:46 +0100

    disable save when not change made

--- src/components/HealthFacilityForm.js
+++ src/components/HealthFacilityForm.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { injectIntl } from "react-intl";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import _ from "lodash";
 
 import ReplayIcon from "@material-ui/icons/Replay";
 import { withTheme, withStyles } from "@material-ui/core/styles";
@@ -114,6 +115,7 @@ class HealthFacilityForm extends Component {
   canSave = () => {
     // TODO - after such component is available, add contract dates as date range, not two separate dates
     if (this.state.isSaved) return false;
+    if (_.isEqual(this.props.healthFacility, _.omit(this.state.healthFacility, "parentLocation"))) return false;
     if (!this.state.healthFacility.code) return false;
     if (this.props.isHFCodeValid === false) return false;
     if (!this.state.healthFacility.name) return false;
